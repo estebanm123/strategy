d0c1aeef4b9ad7aec5dfa772da69caad194906cb
Esteban <margaronesteban@gmail.com>
2026-02-16 11:47:22 -0800

    Add turn manager

--- ActorFactory.gd
+++ ActorFactory.gd
@@ -4,16 +4,16 @@ class_name ActorFactory
 const FRIENDLY_SPRITE_PATH: String = "res://sprites/placeholder-friendly.png"
 const ENEMY_SPRITE_PATH: String = "res://sprites/placeholder-enemy.png"
 
-static func createFriendlyActor() -> Actor:
+static func createFriendlyActor(abilityPool: AbilityPool) -> Actor:
 	var texture: Texture2D = load(FRIENDLY_SPRITE_PATH)
 	if not texture:
 		push_error("Failed to load friendly sprite")
 		return null
-	return Actor.new(texture)
+	return Actor.new(texture, abilityPool)
 
-static func createEnemyActor() -> Actor:
+static func createEnemyActor(abilityPool: AbilityPool) -> Actor:
 	var texture: Texture2D = load(ENEMY_SPRITE_PATH)
 	if not texture:
 		push_error("Failed to load enemy sprite")
 		return null
-	return Actor.new(texture)
+	return Actor.new(texture, abilityPool)
--- Main.gd
+++ Main.gd
@@ -3,51 +3,36 @@ extends Node2D
 
 @onready var tileMapWrapper: TileMapWrapper = TileMapWrapper.new($TileMapLayer)
 
+var abilityPool: AbilityPool
+var turnManager: TurnManager
+
 func _ready() -> void:
- var texture: Texture2D = load("res://sprites/greentile.png")
- if not texture:
-  push_error("Failed to load greentile.png")
-  return
+	abilityPool = AbilityPool.new()
+	turnManager = TurnManager.new(tileMapWrapper, abilityPool, self)
+	
+	var texture: Texture2D = load("res://sprites/greentile.png")
+	if not texture:
+		push_error("Failed to load greentile.png")
+		return
 
- tileMapWrapper.createIsometricTileSet(texture)
- tileMapWrapper.fillMap()
- setupCamera()
- spawnEnemies()
+	tileMapWrapper.createIsometricTileSet(texture)
+	tileMapWrapper.fillMap()
+	setupCamera()
+	turnManager.spawnEnemies()
 
-
+func _unhandled_input(event: InputEvent) -> void:
+	turnManager.handleInput(event)
 
 func setupCamera() -> void:
- var camera: Camera2D = Camera2D.new()
- camera.zoom = Vector2(4, 4)
- add_child(camera)
+	var camera: Camera2D = Camera2D.new()
+	camera.zoom = Vector2(4, 4)
+	add_child(camera)
 
- var rect: Rect2i = tileMapWrapper.tileMapLayerRef.get_used_rect()
- if rect.size.x == 0:
-  return
+	var rect: Rect2i = tileMapWrapper.tileMapLayerRef.get_used_rect()
+	if rect.size.x == 0:
+		return
 
- var startPos: Vector2 = tileMapWrapper.tileMapLayerRef.map_to_local(rect.position)
- var endPos: Vector2 = tileMapWrapper.tileMapLayerRef.map_to_local(rect.end - Vector2i(1, 1))
+	var startPos: Vector2 = tileMapWrapper.tileMapLayerRef.map_to_local(rect.position)
+	var endPos: Vector2 = tileMapWrapper.tileMapLayerRef.map_to_local(rect.end - Vector2i(1, 1))
 
- camera.position = (startPos + endPos) / 2.0
-
-
-func spawnEnemies() -> void:
- var enemyCount: int = randi_range(1, 3)
- var spawnColumn: int = 0
- var maxRows: int = 4
- 
- var availableRows: Array = []
- for y: int in range(maxRows):
-  availableRows.append(y)
- 
- availableRows.shuffle()
- 
- for i: int in range(enemyCount):
-  var enemy: Actor = ActorFactory.createEnemyActor()
-  if not enemy:
-   continue
-  
-  var tilePos: Vector2i = Vector2i(spawnColumn, availableRows[i])
-  var worldPos: Vector2 = tileMapWrapper.tileMapLayerRef.map_to_local(tilePos)
-  enemy.position = worldPos
-  add_child(enemy)
+	camera.position = (startPos + endPos) / 2.0
